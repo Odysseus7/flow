b971a3fda2b0b5fbd63cbc47acb878df83baf306
Odysseus7 <chalitandunu130@gmail.com>
2021-10-27 20:19:18 +0200

    reinit backend

---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports  = {
+    mongoURI: "mongodb+srv://root:root@cluster0.hrhid.mongodb.net/udemy?retryWrites=true&w=majority"
+}
---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const keys = require ("./config/keys");
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
