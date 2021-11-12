74c0ec30132c76d45f6dbde4b2dfb5b012e6f09c
Odysseus7 <chalitandunu130@gmail.com>
2021-11-12 15:37:45 +0100

    implement darkmode

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { GlobalStyles } from "./theme/GlobalStyles";
-import { lightTheme, darkTheme } from "./theme/Themes"
+// import { GlobalStyles } from "./theme/GlobalStyles";
+// import { lightTheme, darkTheme } from "./theme/Themes"
 import ProjectList from "./projects/ProjectList";
 import { darkModeOn } from "../actions";
 import { darkModeOff } from "../actions";
-import { ThemeProvider } from "styled-components";
+// import { ThemeProvider } from "styled-components";
 import ThemeButton from './theme/ThemeButton';
 import "../scss/style.scss";
 import Home from './home/Home';
@@ -15,14 +15,14 @@ class App extends Component {
     render() {
         const darkMode = JSON.parse(window.localStorage.getItem("darkMode"));
         return (
-            <ThemeProvider theme={ darkMode ? darkTheme : lightTheme}>
-                <GlobalStyles />
+            <div class="App" data-theme={darkMode ? "dark" : "light"} >
+                {/* <GlobalStyles /> */}
                     <ThemeButton onClick={ darkMode ? this.props.darkModeOff : this.props.darkModeOn } darkMode={this.props.darkMode} />
                     <BrowserRouter>
                         <Route path="/" exact component={Home} />
                         <Route path="/projects" component={ProjectList} />
                     </BrowserRouter>
-            </ThemeProvider>
+            </div>
         );
     }
 }
