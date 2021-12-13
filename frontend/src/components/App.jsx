c5ec796acf7a7886a429ade84f99d84a323bead2
Odysseus7 <chalitandunu130@gmail.com>
2021-12-13 17:45:41 +0100

    finish setup page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,52 +1,62 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import ProjectList from "./projects/ProjectList";
 import CourseList from "./courses/CourseList";
 import { darkModeOn } from "../actions";
 import { darkModeOff } from "../actions";
-import ThemeButton from './theme/ThemeButton';
+import ThemeButton from "./theme/ThemeButton";
 import "../scss/style.scss";
-import Home from './home/Home';
-import BookList from './books/BookList';
-import About from './about/About';
+import Home from "./home/Home";
+import BookList from "./books/BookList";
+import About from "./about/About";
+import Setup from "./setup/Setup";
 
 class App extends Component {
-    setDarkMode() {
-        this.darkMode = JSON.parse(window.localStorage.getItem("darkMode"));
-        var body = document.querySelector('body');
-        body.setAttribute("data-theme", `${this.darkMode ? "dark" : "light"}`);
-    }
-    
+	setDarkMode() {
+		this.darkMode = JSON.parse(window.localStorage.getItem("darkMode"));
+		var body = document.querySelector("body");
+		body.setAttribute("data-theme", `${this.darkMode ? "dark" : "light"}`);
+	}
 
-    render() {
-        this.setDarkMode();
-        const logo = <a href="/"><img src="../images/logo.svg" className="logo" alt="Logo" /></a>;
+	render() {
+		this.setDarkMode();
+		const logo = (
+			<a href="/">
+				<img src="../images/logo.svg" className="logo" alt="Logo" />
+			</a>
+		);
 
-        return (
-            <div className="app">
-                <header className="app__header">
-                    {window.location.pathname === '/' ? "" : logo}
-                    <ThemeButton onClick={ this.darkMode ? this.props.darkModeOff : this.props.darkModeOn } darkMode={this.props.darkMode} />
-                </header>
-                
-                <BrowserRouter>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about" exact component={About} />
-                    <Route path="/projects" exact component={ProjectList} />
-                    <Route path="/courses" exact component={CourseList} />
-                    <Route path="/books" exact component={BookList} />
-                </BrowserRouter>
-            </div>
-        );
-    }
+		return (
+			<div className="app">
+				<header className="app__header">
+					{window.location.pathname === "/" ? "" : logo}
+					<ThemeButton
+						onClick={
+							this.darkMode ? this.props.darkModeOff : this.props.darkModeOn
+						}
+						darkMode={this.props.darkMode}
+					/>
+				</header>
+
+				<BrowserRouter>
+					<Route path="/" exact component={Home} />
+					<Route path="/about" exact component={About} />
+					<Route path="/projects" exact component={ProjectList} />
+					<Route path="/courses" exact component={CourseList} />
+					<Route path="/books" exact component={BookList} />
+					<Route path="/setup" exact component={Setup} />
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    return {
-        ...state,
-        darkMode: state.darkMode
-    }
-} 
+	return {
+		...state,
+		darkMode: state.darkMode,
+	};
+};
 
 export default connect(mapStateToProps, { darkModeOn, darkModeOff })(App);
--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 import aboutPicture from "../../images/me.svg";
 
 function About() {
-    return (
-        <section className="main main__about">
-            <main className="about__container">
-                <article className="about__picture">
-                    <img src={aboutPicture} alt="Picture of me" />
-                </article>
-                <article className="about__info">
-                    <h1 className="about__heading">Chalita Ndunu</h1>
-                    <p className="about__description">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, voluptatibus?
-                        I aspire to connect people through superior design and exceptional projects. I believe that great results are only achieved by combining great ideas. So let's have a coffee!
-                    </p>
-                </article>
-            </main>
-        </section>
-    );
+	return (
+		<section className="main main__about">
+			<main className="about__container">
+				<article className="about__picture">
+					<img src={aboutPicture} alt="pic of me" />
+				</article>
+				<article className="about__info">
+					<h1 className="about__heading">Chalita Ndunu</h1>
+					<p className="about__description">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto,
+						voluptatibus? I aspire to connect people through superior design and
+						exceptional projects. I believe that great results are only achieved
+						by combining great ideas. So let's have a coffee!
+					</p>
+				</article>
+			</main>
+		</section>
+	);
 }
 
 export default About;
