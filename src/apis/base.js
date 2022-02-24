13dbc2090efef7c1acc63543659ffe52b1b5a0d5
Odysseus7 <chalitandunu130@gmail.com>
2022-02-24 11:08:32 +0100

    set api version in env

--- src/apis/base.js
+++ src/apis/base.js
@@ -8,7 +8,7 @@ export const admin = axios.create({
 });
 
 export default axios.create({
-	baseURL: `${process.env.REACT_APP_API_BASE}/api/v1`,
+	baseURL: `${process.env.REACT_APP_API_BASE}/${process.env.REACT_APP_API_VERSION}`,
 	headers: {
 		"Content-Type": "application/json",
 		"x-access-token": localStorage.getItem("token"),
